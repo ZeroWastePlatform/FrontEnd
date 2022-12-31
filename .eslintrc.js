7d1fd5b2714f68ad77f8b18d2bf04dbb466957fa
김상두 <puki4416@gmail.com>
2022-12-31 22:54:08 +0900

    chore : eslint script 추가 및 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,35 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, {
-      extensions: [".ts", ".tsx"],
-      "@typescript-eslint/no-var-requires": 0
-    }]
-  }
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".ts", ".tsx"],
+      },
+    ],
+    "@typescript-eslint/no-var-requires": 0,
+  },
+  settings: {
+    react: {
+      version: "^18.2.0",
+    },
+  },
 };
