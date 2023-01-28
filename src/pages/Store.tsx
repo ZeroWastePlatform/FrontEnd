7438658f6506531e486182defdb90f1ab5d6c658
김상두 <puki4416@gmail.com>
2023-01-28 17:49:02 +0900

    fix: store 기존의 페이지 삭제

--- src/pages/Store.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HeaderContainer from "../components/Common/Header/HeaderContainer";
-
-function Store() {
-  return <HeaderContainer />;
-}
-
-export default Store;
