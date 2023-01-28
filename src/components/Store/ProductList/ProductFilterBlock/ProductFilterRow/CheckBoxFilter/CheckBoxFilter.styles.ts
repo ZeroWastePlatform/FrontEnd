586fd6f8712cbbcd289c7ffb03d8cfd16ecab5a2
김상두 <puki4416@gmail.com>
2023-01-28 17:28:34 +0900

    feat: Store/ProductList/ProductFilterBlock/ProductFilterRow/Checkbox ui, style story 구현

---
+++ src/components/Store/ProductList/ProductFilterBlock/ProductFilterRow/CheckBoxFilter/CheckBoxFilter.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CheckBoxFilterLayout = styled.li`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+`;
+
+export const CheckBoxFilterInput = styled.input``;
+
+export const CheckBoxFilterLabel = styled.label``;
