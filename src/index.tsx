d51436e4274914f10e5a35a8982d5adb8ba4c2ee
김상두 <puki4416@gmail.com>
2023-01-28 17:16:54 +0900

    feat: react-query dev-tools 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,17 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <RecoilRoot>
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </React.StrictMode>
-  </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  </RecoilRoot>,
 );
