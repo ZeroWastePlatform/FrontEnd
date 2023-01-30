bea3698388e702f22ad3b8d9df2f94ee9f727cf8
dngur9801 <dngur98012@naver.com>
2023-01-30 22:40:42 +0900

    feat : Header/HeaderSearch storybook 작성

--- src/components/Common/Header/HeaderSearch/HeaderSearch.styles.ts
+++ src/components/Common/Header/HeaderSearch/HeaderSearch.styles.ts
@@ -2,14 +2,16 @@ import styled from "styled-components";
 import searchImg from "../../../../assets/images/search-icon.png";
 
 export const HeaderSearchLayout = styled.div`
+  width: 475px;
   position: relative;
 `;
 
 export const HeaderSearchInput = styled.input`
-  width: 475px;
+  width: 100%;
   border-radius: 30px;
   border: 1px solid #787878;
   padding: 12px 50px 12px 20px;
+  box-sizing: border-box;
 `;
 
 export const HeaderSearchImg = styled.img.attrs({
