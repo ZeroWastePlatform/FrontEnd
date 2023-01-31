78d647044d4b3d56b406a1cac33075e0c699357e
dngur9801 <dngur98012@naver.com>
2023-02-01 02:10:57 +0900

    fix : Agree 체크박스 전체선택 기능 구현

--- src/components/Signup/Agree/Agree.tsx
+++ src/components/Signup/Agree/Agree.tsx
@@ -14,28 +14,43 @@ import {
   AgreeSelectText,
 } from "./Agree.styles";
 
+type AgreeList = {
+  id: number;
+  text: string;
+  description: string;
+}[];
 interface AgreeProps {
-  agreeList: {
-    text: string;
-    description: string;
-  }[];
+  agreeList: AgreeList;
+  checkedList: AgreeList;
+  onCheckedAll: (checked: any) => void;
+  onCheckedElement: (checked: any, list: any) => void;
 }
 
-const Agree = ({ agreeList }: AgreeProps) => {
+const Agree = ({ agreeList, checkedList, onCheckedAll, onCheckedElement }: AgreeProps) => {
   return (
     <AgreeLayout>
       <AgreeTitle>약관동의</AgreeTitle>
       <AgreeBox>
         <AgreeSelectAll>
-          <AgreeSelectAllInput type="checkbox" id="all" />
+          <AgreeSelectAllInput
+            type="checkbox"
+            id="all"
+            onChange={(e: any) => onCheckedAll(e.target.checked)}
+            checked={checkedList.length === 0 ? false : checkedList.length === agreeList.length ? true : false}
+          />
           <AgreeSelectAllLabel htmlFor="all">전체동의</AgreeSelectAllLabel>
           <AgreeSelectAllText> (선택항목에 대한 동의 포함)</AgreeSelectAllText>
         </AgreeSelectAll>
         <AgreeSelectList>
-          {agreeList.map((item, idx) => (
-            <AgreeSelectItem key={idx}>
-              <AgreeSelectInput type="checkbox" id={"ck" + idx} />
-              <AgreeSelectLabel htmlFor={"ck" + idx}>{item.text}</AgreeSelectLabel>
+          {agreeList.map(item => (
+            <AgreeSelectItem key={item.id}>
+              <AgreeSelectInput
+                type="checkbox"
+                id={"ck" + item.id}
+                onChange={(e: any) => onCheckedElement(e.target.checked, item)}
+                checked={checkedList.includes(item as never) ? true : false}
+              />
+              <AgreeSelectLabel htmlFor={"ck" + item.id}>{item.text}</AgreeSelectLabel>
               <AgreeSelectText color={item.description === "(선택)" ? "#A7A7A8" : "inherit"}>
                 {item.description}
               </AgreeSelectText>
--- src/components/Signup/Agree/AgreeContainer.tsx
+++ src/components/Signup/Agree/AgreeContainer.tsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Agree from "./Agree";
 
 const agreeList = [
   {
+    id: 1,
     text: "만 14세 이상입니다.",
     description: "(필수)",
   },
   {
+    id: 2,
     text: "이용약관 동의",
     description: "(필수)",
   },
   {
+    id: 3,
     text: "개인정보수집 및 이용동의",
     description: "(필수)",
   },
   {
+    id: 4,
     text: "개인정보 마케팅 활용동의",
     description: "(선택)",
   },
 ];
 
 const AgreeContainer = () => {
-  return <Agree agreeList={agreeList} />;
+  const [checkedList, setCheckedLists] = useState<typeof agreeList>([]);
+
+  const onCheckedAll = (checked: any) => {
+    if (checked) {
+      setCheckedLists([...agreeList]);
+    } else {
+      setCheckedLists([]);
+    }
+  };
+
+  const onCheckedElement = (checked: any, list: any) => {
+    if (checked) {
+      setCheckedLists([...checkedList, list]);
+    } else {
+      setCheckedLists(checkedList.filter(el => el !== list));
+    }
+  };
+  return (
+    <Agree
+      agreeList={agreeList}
+      checkedList={checkedList}
+      onCheckedAll={onCheckedAll}
+      onCheckedElement={onCheckedElement}
+    />
+  );
 };
 
 export default AgreeContainer;
