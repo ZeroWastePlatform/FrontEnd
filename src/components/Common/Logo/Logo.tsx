1efa17528114294a2116b6f7cb114699cdf9f1db
dngur9801 <dngur98012@naver.com>
2023-02-01 00:30:48 +0900

    feat : login 페이지와 signup 페이지에서 사용할 공통 컴포넌트 Logo 구현

---
+++ src/components/Common/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CharactersImg, LogoImg, LogoImgBox } from "./Logo.styles";
+
+const Logo = () => {
+  return (
+    <LogoImgBox>
+      <CharactersImg />
+      <LogoImg />
+    </LogoImgBox>
+  );
+};
+
+export default Logo;
