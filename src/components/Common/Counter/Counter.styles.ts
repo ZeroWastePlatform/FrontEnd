65ea9e192232c465ab8953aff615e3c38c2b4a95
김상두 <puki4416@gmail.com>
2023-02-20 19:38:04 +0900

    feat : Counter 컴포넌트 구현

---
+++ src/components/Common/Counter/Counter.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CounterLayout = styled.div`
+  display: flex;
+`;
+
+export const CounterItem = styled.div`
+  width: 25px;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+`;
