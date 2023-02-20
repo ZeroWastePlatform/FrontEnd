0b686abd5e51bc789d18378f3e647748c91c12f0
김상두 <puki4416@gmail.com>
2023-02-20 19:38:43 +0900

    fix : CategoryNavigation 수정

--- src/components/Store/Common/CategoryNavigation/CategoryNavigation.tsx
+++ src/components/Store/Common/CategoryNavigation/CategoryNavigation.tsx
@@ -27,7 +27,14 @@ const CategoryNavigation = ({ condition, setCondition }: CategoryNavigationProps
           "이벤트",
         ].map(category => (
           <CategoryNavigationItem key={category} same={category === condition.category}>
-            <Link to={`/store?category=${category}`} onClick={() => setCondition({ ...condition, category })}>
+            <Link
+              to={`/store?category=${category}`}
+              onClick={() =>
+                setCondition(
+                  category == "베스트" ? { ...condition, category, sort: "식품" } : { ...condition, category },
+                )
+              }
+            >
               {category}
             </Link>
           </CategoryNavigationItem>
