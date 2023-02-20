bb5a571e1b9dd135180bda03621a22e518cd3df7
김상두 <puki4416@gmail.com>
2023-02-20 19:42:25 +0900

    feat : buyButton 컴포넌트 구현

---
+++ src/components/Store/ProductBuy/SlideBox/BuyButton/BuyButton.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BuyButtonLayout = styled.button`
+  background: #66f095;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  margin: 20px 0 0 0;
+  border-radius: 10px;
+  width: 100%;
+  border: none;
+`;
+
+export const BuyButtonText = styled.div`
+  color: white;
+  font-size: 24px;
+  font-weight: 700;
+`;
