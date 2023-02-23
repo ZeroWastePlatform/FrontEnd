8fdedf2d1841595a7f3745db86222f987fdf2915
dngur9801 <dngur98012@naver.com>
2023-02-24 00:12:03 +0900

    feat : Write 컴포넌트 구현

---
+++ src/components/Community/Write/Write.styles.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const WriteLayout = styled.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  margin-top: 60px;
+`;
+
+export const WriteMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+export const WriteBtnBox = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const WriteTempDataBtn = styled.button`
+  border: 1px solid ${({ theme }) => theme.colors.gray4};
+  background-color: white;
+  border-radius: 20px;
+  padding: 8px 15px;
+`;
+
+export const WriteRegistBtn = styled.button`
+  background-color: ${({ theme }) => theme.colors.primaryGreen100};
+  color: white;
+  border-radius: 20px;
+  border: 0;
+  padding: 8px 15px;
+`;
+
+export const WriteSubjectInput = styled.input`
+  width: 100%;
+  height: 40px;
+  border: 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray2};
+  color: ${({ theme }) => theme.colors.gray4};
+  font-size: 24px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray4};
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const WriteContent = styled.textarea`
+  width: 100%;
+  height: 50vh;
+  border: 0;
+  margin-top: 20px;
+  resize: none;
+  color: ${({ theme }) => theme.colors.gray4};
+  font-size: 18px;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray4};
+  }
+`;
+
+export const WriteOptionBox = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
---
+++ src/components/Community/Write/Write.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import CustomSelect from "../../Common/CustomSelect/CustomSelect";
+import AttachmentContainer from "./Attachment/AttachmentContainer";
+import HashtagBoxContainer from "./HashtagBox/HashtagBoxContainer";
+import {
+  WriteBtnBox,
+  WriteContent,
+  WriteLayout,
+  WriteMenu,
+  WriteOptionBox,
+  WriteRegistBtn,
+  WriteSubjectInput,
+  WriteTempDataBtn,
+} from "./Write.styles";
+
+const Write = () => {
+  return (
+    <WriteLayout>
+      <WriteMenu>
+        <CustomSelect initialValue="게시판 선택" options={["자유게시판", "중고거래", "정보공유"]} />
+        <WriteBtnBox>
+          <WriteTempDataBtn>임시저장 | 0</WriteTempDataBtn>
+          <WriteRegistBtn>등록</WriteRegistBtn>
+        </WriteBtnBox>
+      </WriteMenu>
+      <WriteSubjectInput placeholder="제목을 입력해주세요." />
+      <WriteContent placeholder="내용을 입력해주세요."></WriteContent>
+      <WriteOptionBox>
+        <AttachmentContainer />
+        <HashtagBoxContainer />
+      </WriteOptionBox>
+    </WriteLayout>
+  );
+};
+
+export default Write;
