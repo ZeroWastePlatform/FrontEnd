08237e8d074dab44c1aa337b953182557dba1b93
김상두 <puki4416@gmail.com>
2023-03-01 21:25:10 +0900

    fix : payMethod 디자인 수정

--- src/components/Store/ProductBuy/PayMethod/PayMethod.styles.ts
+++ src/components/Store/ProductBuy/PayMethod/PayMethod.styles.ts
@@ -9,16 +9,24 @@ export const PayMethodPayBoxList = styled.div`
   justify-content: center;
 `;
 
-export const PayMethodPayBox = styled.div<{ selected: boolean }>`
+export const PayMethodPayBox = styled.div<{ selectedIndex: number; curindex: number }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 10px;
-  width: 100px;
-  height: 100px;
-  background: ${props => (props.selected ? "#F0FEF4" : "#FFFFFF")};
-  border: ${props => (props.selected ? "1px solid #66F095" : "1px solid #D3D3D4")};
+  width: 120px;
+  height: 120px;
+  background: ${({ selectedIndex, curindex }) => (selectedIndex === curindex ? "#F0FEF4" : "#FFFFFF")};
+  border: ${({ selectedIndex, curindex }) => (selectedIndex === curindex ? "2px solid #66F095" : "1px solid #D3D3D4")};
+  font-size: 16px;
+  font-weight: 500;
+  border-left: ${({ selectedIndex, curindex }) =>
+    selectedIndex === curindex ? "2px solid #66F095" : curindex === 0 ? "1px solid #D3D3D4" : "none"};
+  border-right: ${({ selectedIndex, curindex }) =>
+    selectedIndex === curindex ? "2px solid #66F095" : selectedIndex - 1 !== curindex ? "1px solid #D3D3D4" : "none"};
+  font-size: 16px;
+  cursor: pointer;
 `;
 
 export const PayMethodImg = styled.img`
