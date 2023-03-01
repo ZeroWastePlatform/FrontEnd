e37d6138d91cb08b4b409ab0f9a5f0c6b588aae4
dngur9801 <dngur98012@naver.com>
2023-03-01 22:27:10 +0900

    feat : Article 컴포넌트 구현

---
+++ src/pages/Community/Article.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import CategoryNavigation from "../../components/Community/Common/CommunityHeader/CategoryNavigation/CategoryNavigation";
+import styled from "styled-components";
+import AsideContainer from "../../components/Community/Article/Aside/AsideContainer";
+import ContentBoxContainer from "../../components/Community/Article/ContentBox/ContentBoxContainer";
+import CommentBoxContainer from "../../components/Community/Article/CommentBox/CommentBoxContainer";
+
+const Article = () => {
+  return (
+    <>
+      <CategoryNavigation />
+      <ArticleLayout>
+        <ArticleBox>
+          <ContentBoxContainer />
+          <CommentBoxContainer />
+        </ArticleBox>
+        <AsideContainer />
+      </ArticleLayout>
+    </>
+  );
+};
+
+const ArticleLayout = styled.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
+`;
+const ArticleBox = styled.div``;
+
+export default Article;
