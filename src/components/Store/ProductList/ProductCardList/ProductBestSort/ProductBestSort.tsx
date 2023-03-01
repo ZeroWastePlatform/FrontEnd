64e3a0beac266fad0dbbb40471efeb9ccbf6bc85
김상두 <puki4416@gmail.com>
2023-03-01 21:17:49 +0900

    refac : sort 관련 컴포넌트 리팩토링

--- src/components/Store/ProductList/ProductCardList/ProductBestSort/ProductBestSort.tsx
+++ src/components/Store/ProductList/ProductCardList/ProductBestSort/ProductBestSort.tsx
@@ -1,21 +1,42 @@
 import { conditionType, setConditionType } from "../../../../../pages/Store/ProductList";
-import { ProductBestSortLayout, ProductBestSortOption, ProductBestSortSelect } from "./ProductBestSort.styles";
-
+import {
+  ProductBestSortButton,
+  ProductBestSortItem,
+  ProductBestSortLayout,
+  ProductBestSortList,
+} from "./ProductBestSort.styles";
+import { theme } from "../../../../../styles/theme";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+import bottomArrow from "../../../../../assets/images/bottomArrow.svg";
 interface ProductBestSortProps {
   condition: conditionType;
   setCondition: setConditionType;
 }
 
 const ProductBestSort = ({ condition, setCondition }: ProductBestSortProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <ProductBestSortLayout>
-      <ProductBestSortSelect onChange={e => setCondition({ ...condition, sort: e.target.value })}>
-        {["식품", "주방", "욕실", "생활", "취미", "선물", "여성용품", "반려동물", "문구"].map(e => (
-          <ProductBestSortOption key={e}>{e}</ProductBestSortOption>
-        ))}
-      </ProductBestSortSelect>
-    </ProductBestSortLayout>
+    <ThemeProvider theme={theme}>
+      <ProductBestSortLayout>
+        <ProductBestSortButton onClick={() => setIsOpen(!isOpen)}>
+          {condition.sort} <img src={bottomArrow} />
+        </ProductBestSortButton>
+        {isOpen ? (
+          <ProductBestSortList>
+            {bestSortData.map(element => (
+              <ProductBestSortItem key={element} onClick={() => setCondition({ ...condition, sort: element })}>
+                {element}
+              </ProductBestSortItem>
+            ))}
+          </ProductBestSortList>
+        ) : null}
+      </ProductBestSortLayout>
+    </ThemeProvider>
   );
 };
 
 export default ProductBestSort;
+
+const bestSortData = ["식품", "주방", "욕실", "생활", "취미", "선물", "여성용품", "반려동물", "문구"];
