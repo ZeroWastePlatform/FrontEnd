20da73360a044439091fd0de1b15ce0ba0310e44
김상두 <puki4416@gmail.com>
2023-03-01 21:20:42 +0900

    feat : pagenation 기능 구현

---
+++ src/components/Common/Pagenation/PagenationContainer.tsx
@@ -0,0 +1,43 @@
+import Pagenation from "./Pagenation";
+
+interface PagenationContainerProps {
+  page: number;
+  totalPage: number;
+  unit: number;
+  setPage: (page: number) => void;
+}
+const PagenationContainer = ({ page, setPage, totalPage, unit }: PagenationContainerProps) => {
+  const makePageList = () => {
+    const unitPage = Math.ceil(page / unit);
+    const startPage = (unitPage - 1) * 9 + 1;
+    const list = new Array(9)
+      .fill(0)
+      .map((_, index) => index + startPage)
+      .filter(page => page <= totalPage);
+    return list;
+  };
+
+  const checkMoveLeft = () => {
+    const unitPage = Math.ceil(page / unit);
+    if (unitPage === 1) return false;
+    return true;
+  };
+
+  const checkMoveRight = () => {
+    const unitPage = Math.ceil(page / unit);
+    if (unitPage * unit + 1 <= totalPage) return true;
+    return false;
+  };
+
+  return (
+    <Pagenation
+      page={page}
+      moveLeft={checkMoveLeft()}
+      moveRight={checkMoveRight()}
+      pageList={makePageList()}
+      setPage={setPage}
+    ></Pagenation>
+  );
+};
+
+export default PagenationContainer;
