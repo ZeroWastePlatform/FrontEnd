fc42a9cc818a63e7a6eeb6b03d954346430d2b4a
dlantjdgkgk <dlantjdgkgk@naver.com>
2023-03-05 19:04:09 +0900

    feat : 마이페이지 api 연동

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { apiInstance } from "../../api/setting";
 import CategoryNavigation from "./Common/CategoryNavigation/CategoryNavigation";
 import ProfileAside from "./Common/ProfileAside/ProfileAside";
 import { MyPageRow } from "./MyPage.style";
@@ -16,6 +18,17 @@ const MyPage = ({ selectCategoryComponentList }: IMyPageProps) => {
   const categoryNav = ["나의 주문", "관심상품", "커뮤니티", "컨텐츠", "회원정보설정"];
   const defaultSelect = categoryNav[0];
   const [selectCategoryNav, setSelectCategoryNav] = useState(defaultSelect);
+  const [memberInfo, setMemberInfo] = useState();
+
+  const membersGetAPI = async (): Promise<void> => {
+    const result = await axios.get("");
+    console.log(result);
+    // setLocalPopularGifticons(result.data.data.topShopByLocate);
+  };
+
+  useEffect(() => {
+    membersGetAPI();
+  }, []);
 
   return (
     <>
