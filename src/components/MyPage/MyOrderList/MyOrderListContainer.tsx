417d418d799ba2a7c12fb322fa078a591f577909
dlantjdgkgk <dlantjdgkgk@naver.com>
2023-03-05 19:03:26 +0900

    feat : 나의 주문 내역 부분 구현

--- src/components/MyPage/MyOrderList/MyOrderListContainer.tsx
+++ src/components/MyPage/MyOrderList/MyOrderListContainer.tsx
@@ -7,11 +7,23 @@ const selectList = [
   },
   {
     id: 2,
-    period: "1주 ",
+    period: "1개월 전 ",
   },
   {
     id: 3,
-    period: "2주 ",
+    period: "3개월 전 ",
+  },
+  {
+    id: 4,
+    period: "6개월 전 ",
+  },
+  {
+    id: 5,
+    period: "1년 전 ",
+  },
+  {
+    id: 6,
+    period: "3년 전 ",
   },
 ];
 
