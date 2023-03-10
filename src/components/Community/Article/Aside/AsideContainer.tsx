d4d8bf2e2c5d06107aeb8fad9fbdab7edd476c47
dngur9801 <dngur98012@naver.com>
2023-03-10 21:34:17 +0900

    feat : Aside컴포넌트 api 연동

--- src/components/Community/Article/Aside/AsideContainer.tsx
+++ src/components/Community/Article/Aside/AsideContainer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import useSuspenseQuery from "../../../../hooks/useSuspenseQuery";
+import { PopularityPostType, RecommendationsPostType } from "../../../../types";
 import Aside from "./Aside";
 
-const AsideContainer = () => {
-  return <Aside />;
+interface AsideContainerProps {
+  kind: number;
+}
+
+const AsideContainer = ({ kind }: AsideContainerProps) => {
+  const { data: recommendations } = useSuspenseQuery<RecommendationsPostType[]>(
+    ["Community", "Article", "Aside", "recommendations"],
+    `posts/recommendations/${kind}`,
+  );
+  const { data: popularity } = useSuspenseQuery<PopularityPostType[]>(
+    ["Community", "Article", "Aside", "popularity"],
+    `posts/popularity`,
+  );
+  return <Aside recommendations={recommendations} popularity={popularity} />;
 };
 
 export default AsideContainer;
