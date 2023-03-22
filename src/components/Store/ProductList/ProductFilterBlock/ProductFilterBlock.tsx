94715b8ee242fc3939257eba163eda59632a02ed
김상두 <puki4416@gmail.com>
2023-03-22 15:49:23 +0900

    fix : filtering 방식 변경에 따른 productFilterBlock의 함수 수정 컴포넌트 수정

--- src/components/Store/ProductList/ProductFilterBlock/ProductFilterBlock.tsx
+++ src/components/Store/ProductList/ProductFilterBlock/ProductFilterBlock.tsx
@@ -1,71 +1,29 @@
-import { conditionType, filterType } from "../../../../pages/Store/ProductList";
+import { conditionType } from "../../../../pages/Store/ProductList";
 import { ProductFilterBlockCol, ProductFilterBlockLayout } from "./ProductFilterBlock.styles";
+import { filter } from "./ProductFilterBlockContainer";
+import filterList from "./ProductFilterData";
 import StoreFilterRow from "./ProductFilterRow/ProductFilterRow";
 import SelectedProductFilterList from "./SelectedProductFilterList/SelectedProductFilterList";
 
 interface ProductFilterBlockProps {
   condition: conditionType;
-  setCheckboxFilter: (clickedFilter: filterType) => void;
-  setRadioFilter: (clickedFilter: filterType) => void;
-  deleteFilter: (clickedFilter: filterType) => void;
-  selectedCheck: (text: string) => boolean;
-  resetFilter: () => void;
+  setFilter: (clickedFilter: filter) => void;
+  isSelected: (name: string, value: string) => boolean;
 }
 
-const ProductFilterBlock = ({
-  condition,
-  setCheckboxFilter,
-  setRadioFilter,
-  deleteFilter,
-  selectedCheck,
-  resetFilter,
-}: ProductFilterBlockProps) => {
+const ProductFilterBlock = ({ condition, setFilter, isSelected }: ProductFilterBlockProps) => {
   return (
     <ProductFilterBlockLayout>
       <ProductFilterBlockCol>
-        <StoreFilterRow
-          title={"브랜드"}
-          list={[
-            { text: "잇츠베러", value: "0" },
-            { text: "젤러스 스윗", value: "1" },
-            { text: "비건 포레스트", value: "2" },
-            { text: "저스트에그", value: "3" },
-            { text: "비건타민", value: "4" },
-            { text: "바오푸드", value: "5" },
-            { text: "로마린다", value: "6" },
-          ]}
-          selectedCheck={selectedCheck}
-          setCheckboxFilter={setCheckboxFilter}
-          setRadioFilter={setRadioFilter}
-        />
-        <StoreFilterRow
-          title={"가격"}
-          list={[
-            { text: "10000원이하", value: "7", name: "price" },
-            { text: "10000~30000", value: "8", name: "price" },
-            { text: "30000~50000", value: "9", name: "price" },
-            { text: "50000이상", value: "10", name: "price" },
-          ]}
-          selectedCheck={selectedCheck}
-          setCheckboxFilter={setCheckboxFilter}
-          setRadioFilter={setRadioFilter}
-        />
-        <StoreFilterRow
-          title={"제품상태"}
-          list={[
-            { text: "무료배송", value: "11" },
-            { text: "할인 상품", value: "12" },
-            { text: "품절상품 제외", value: "13" },
-          ]}
-          selectedCheck={selectedCheck}
-          setCheckboxFilter={setCheckboxFilter}
-          setRadioFilter={setRadioFilter}
-        />
-        <SelectedProductFilterList
+        {filterList.map(({ title, list }) => (
+          <StoreFilterRow title={title} list={list} setFilter={setFilter} key={title} isSelected={isSelected} />
+        ))}
+        {/* TO-DO : 삭제필터를 삭제해도 된다고 하면 코드 제거하기 */
+        /* <SelectedProductFilterList
           selectedFilters={condition.filter}
           deleteFilter={deleteFilter}
           resetFilter={resetFilter}
-        />
+        /> */}
       </ProductFilterBlockCol>
     </ProductFilterBlockLayout>
   );
--- src/components/Store/ProductList/ProductFilterBlock/ProductFilterBlockContainer.tsx
+++ src/components/Store/ProductList/ProductFilterBlock/ProductFilterBlockContainer.tsx
@@ -1,65 +1,62 @@
-import { conditionType, filterType, setConditionType } from "../../../../pages/Store/ProductList";
+import { conditionType, setConditionType } from "../../../../pages/Store/ProductList";
 import ProductFilterBlock from "./ProductFilterBlock";
 
+export type filter = { text: string; value: string; name: string };
 interface ProductFilterBlockProps {
   condition: conditionType;
   setCondition: setConditionType;
 }
 
 const ProductFilterBlockContainer = ({ condition, setCondition }: ProductFilterBlockProps) => {
-  const setCheckboxFilter = (clickedFilter: filterType) => {
-    const filtered = condition.filter.filter(el => {
-      if (el.value === clickedFilter.value) return false;
-      return true;
-    });
+  /* TO-DO : 체크박스를 삭제해도 된다고 하면 코드제거하기 */
+  // const setCheckboxFilter = (clickedFilter: filterType) => {
+  //   const filtered = condition.filter.filter(el => {
+  //     if (el.value === clickedFilter.value) return false;
+  //     return true;
+  //   });
 
-    if (filtered.length === condition.filter.length) {
-      setCondition({ ...condition, filter: [...condition.filter, clickedFilter] });
-    } else {
-      setCondition({ ...condition, filter: filtered });
-    }
+  //   if (filtered.length === condition.filter.length) {
+  //     setCondition({ ...condition, filter: [...condition.filter, clickedFilter] });
+  //   } else {
+  //     setCondition({ ...condition, filter: filtered });
+  //   }
+  // };
+
+  const setFilter = (clickedFilter: filter) => {
+    const { name, value } = clickedFilter;
+    setCondition({ ...condition, [name]: condition[name] === value ? null : value });
   };
 
-  const setRadioFilter = (clickedFilter: filterType) => {
-    const filtered = condition.filter.filter(el => {
-      if (el.name === clickedFilter.name) return false;
-      return true;
-    });
+  /* TO-DO : 삭제필터를 삭제해도 된다고 하면 코드 제거하기 */
+  // const deleteFilter = (clickedFilter: filterType) => {
+  //   const filtered = condition.filter.filter(el => {
+  //     if (el.value === clickedFilter.value) return false;
+  //     return true;
+  //   });
+  //   setCondition({ ...condition, filter: [...filtered] });
+  // };
 
-    setCondition({ ...condition, filter: [...filtered, clickedFilter] });
-  };
+  /* TO-DO : 체크박스를 삭제해도 된다고 하면 코드제거하기 */
+  // const selectedCheck = (text: string) => {
+  //   const filtered = condition.filter.filter(el => {
+  //     if (el.text === text) return true;
+  //     return false;
+  //   });
+  //   if (filtered.length === 1) return true;
+  //   return false;
+  // };
 
-  const deleteFilter = (clickedFilter: filterType) => {
-    const filtered = condition.filter.filter(el => {
-      if (el.value === clickedFilter.value) return false;
-      return true;
-    });
-    setCondition({ ...condition, filter: [...filtered] });
-  };
-
-  const selectedCheck = (text: string) => {
-    const filtered = condition.filter.filter(el => {
-      if (el.text === text) return true;
-      return false;
-    });
-    if (filtered.length === 1) return true;
+  const isSelected = (name: string, value: string) => {
+    if (condition[name] === value) return true;
     return false;
   };
 
-  const resetFilter = () => {
-    setCondition({ ...condition, filter: [] });
-  };
+  /* TO-DO : 삭제필터를 삭제해도 된다고 하면 코드 제거하기 */
+  // const resetFilter = () => {
+  //   setCondition({ ...condition, filter: [] });
+  // };
 
-  return (
-    <ProductFilterBlock
-      condition={condition}
-      setCheckboxFilter={setCheckboxFilter}
-      setRadioFilter={setRadioFilter}
-      deleteFilter={deleteFilter}
-      selectedCheck={selectedCheck}
-      resetFilter={resetFilter}
-    />
-  );
+  return <ProductFilterBlock condition={condition} setFilter={setFilter} isSelected={isSelected} />;
 };
 
 export default ProductFilterBlockContainer;
