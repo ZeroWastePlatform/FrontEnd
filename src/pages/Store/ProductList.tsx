25331786bed06760c4655c55d036367ed3b0621d
김상두 <puki4416@gmail.com>
2023-03-22 11:14:49 +0900

    fix : 서버로 요청할 쿼리에 맞춰서 타입 및 키값 수정

--- src/pages/Store/ProductList.tsx
+++ src/pages/Store/ProductList.tsx
@@ -1,58 +1,68 @@
 import React, { Suspense, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import CategoryNavigation from "../../components/Store/Common/CategoryNavigation/CategoryNavigation";
 import ProductCardListContainer from "../../components/Store/ProductList/ProductCardList/ProductCardListContainer";
 import ProductFilterBlockContainer from "../../components/Store/ProductList/ProductFilterBlock/ProductFilterBlockContainer";
 import { ErrorBoundary } from "react-error-boundary";
 import ProductCarouselContainer from "../../components/Common/Carousel/CarouselContainer";
+import makeQuery from "../../utils/makeQuery";
 
-export interface filterType {
-  name: string | null;
-  value: string;
-  text: string;
-}
+type category = "FOOD" | "KITCHEN" | "BATH" | "LIFE" | "HOBBY" | "GIFT" | "WOMAN" | "PET" | "STATIONERY";
+type price = "LT_10" | "BT_10_30" | "BT_30_50" | "GT_50";
+type sort = "POPULARITY" | "NEW" | "LOW_PRICE" | "HIGH_PRICE";
+type status = "FREE_SHIPPING" | "DISCOUNT_STOCK" | "REMOVE_OUT_OF_STOCK";
 export interface conditionType {
-  category: string;
-  filter: filterType[];
-  sort: string;
+  category: category | "ALL" | "TOP6" | null;
+  brand: string | null;
+  price: price | null;
+  status: status | null;
+  sort: sort | category | "TOP6";
   page: number;
 }
 
 export type setConditionType = React.Dispatch<conditionType>;
 
 function Store() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("category");
+
   const [condition, setCondition] = useState<conditionType>({
-    category: "전체",
-    filter: [],
-    sort: "인기순",
+    category: query ? (query as category) : "ALL",
+    brand: null,
+    price: null,
+    status: null,
+    sort: "POPULARITY",
     page: 1,
   });
   const navigate = useNavigate();
 
   useEffect(() => {
-    const filterQuery = `&filter=${condition.filter.map(({ text }) => text).join("%")}`;
-    navigate(
-      `/store?category=${condition.category}${condition.filter.length ? filterQuery : ""}&sort=${condition.sort}&page=${
-        condition.page
-      }`,
-    );
+    const fixQuery = (condition: conditionType) => {
+      const newCondition = { ...condition };
+      if (condition.category === "ALL") {
+        newCondition.category = null;
+      }
+      if (condition.category === "TOP6") {
+        newCondition.category = newCondition.sort as category;
+        newCondition.sort = "TOP6";
+      }
+      return newCondition;
+    };
+    const query = makeQuery(fixQuery(condition));
+    navigate(`/store?${query}`);
   }, [condition]);
-  /*TO-DO : 에러 컴포넌트와 로딩 컴포넌트 제작*/
+
   return (
     <>
       <CategoryNavigation condition={condition} setCondition={setCondition} />
-      {condition.category === "전체" || condition.category === "베스트" ? (
-        <ErrorBoundary FallbackComponent={() => <div>...에러발생</div>}>
-          <Suspense fallback={<div>...로딩중</div>}>
-            <ProductCarouselContainer type="Store" />
-          </Suspense>
-        </ErrorBoundary>
-      ) : null}
-      {condition.category === "전체" || condition.category === "베스트" ? null : (
+      {condition.category === "ALL" || condition.category === "TOP6" ? null : (
         <ProductFilterBlockContainer condition={condition} setCondition={setCondition} />
       )}
-      <ErrorBoundary FallbackComponent={() => <div>...에러발생</div>}>
-        <Suspense fallback={<div style={{ width: "1200px", height: "1908px" }}>...로딩중</div>}>
+      <ErrorBoundary FallbackComponent={() => <div style={exceptCaseStyle}>에러발생</div>}>
+        <Suspense fallback={<div style={exceptCaseStyle}>로딩중</div>}>
+          {condition.category === "ALL" || condition.category === "TOP6" ? (
+            <ProductCarouselContainer type="Store" />
+          ) : null}
           <ProductCardListContainer condition={condition} setCondition={setCondition} />
         </Suspense>
       </ErrorBoundary>
@@ -61,3 +71,13 @@ function Store() {
 }
 
 export default Store;
+
+const exceptCaseStyle = {
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "50px",
+  fontWeight: "600",
+};
