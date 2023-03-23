9f5ad4387a257744be5ff4ecac689aafd20c9b71
김상두 <puki4416@gmail.com>
2023-03-23 13:37:26 +0900

    fix : 코드 스타일 및 console.log 제거

--- src/components/Store/ProductBuy/SlideBox/BuyButton/BuyButtonContainer.tsx
+++ src/components/Store/ProductBuy/SlideBox/BuyButton/BuyButtonContainer.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { isBuyFormAtom, PriceSelector } from "../../../../../atom/buyForm";
 import BuyButton from "./BuyButton";
@@ -5,9 +7,29 @@ import BuyButton from "./BuyButton";
 const BuyButtonContainer = () => {
   const price = useRecoilValue(PriceSelector);
   const buyInfo = useRecoilValue(isBuyFormAtom);
-  const buyProduct = () => {
-    alert("구매됨!");
-    console.log(buyInfo);
+  const navigation = useNavigate();
+  const buyProduct = async () => {
+    const body = {
+      name: buyInfo.ordererName,
+      phoneNum: buyInfo.ordererPhone,
+      email: buyInfo.ordererEmailFirst,
+      address: {
+        addressName: "집",
+        recipient: buyInfo.ordererName,
+        recipientPhone: buyInfo.ordererPhone,
+        zipCode: buyInfo.address1,
+        address: buyInfo.address2,
+        addressDetail: buyInfo.address3,
+      },
+      totalPrice: price + 3000 - buyInfo.point,
+    };
+    try {
+      await axios.post("http://greenus.duckdns.org/api/purchases", body);
+      alert("구매에 성공하였습니다!");
+      navigation("/");
+    } catch {
+      alert("에러가 발생하였습니다. 다시 시도해주세요");
+    }
   };
   return <BuyButton totalPrice={price + 3000 - buyInfo.point} buyProduct={buyProduct} />;
 };
--- src/pages/Store/ProductBuy.tsx
+++ src/pages/Store/ProductBuy.tsx
@@ -3,7 +3,6 @@ import CouponPointContainer from "../../components/Store/ProductBuy/CouponPoint/
 import OrderInfo from "../../components/Store/ProductBuy/OrdererInfo/OrderInfo";
 import OrderProductContainer from "../../components/Store/ProductBuy/OrderProduct/OrderProductContainer";
 import PayMethodContainer from "../../components/Store/ProductBuy/PayMethod/PayMethodContainer";
-import ShipInfo from "../../components/Store/ProductBuy/ShipInfo/ShipInfo";
 import ShipInfoContainer from "../../components/Store/ProductBuy/ShipInfo/ShipInfoContainer";
 import SlideBox from "../../components/Store/ProductBuy/SlideBox/SlideBox";
 
