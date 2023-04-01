6ba33c1203ac64245c7b6421626b4d0b00e99bf9
김상두 <puki4416@gmail.com>
2023-04-01 10:04:29 +0900

    fix : home 의 카테고리 link가 제대로 동작하지 않는 오류수정

--- src/components/Home/ProductCategory/CategoryItem/CategoryItem.tsx
+++ src/components/Home/ProductCategory/CategoryItem/CategoryItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { category } from "../../../../pages/Store/ProductList";
 import { ICategoryList } from "../../../../types/index";
 import { CategoryItemImg, CategoryItemLayout, CategoryItemText } from "./CategoryItem.styles";
 
@@ -8,7 +9,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category: { image, text } }: CategoryItemProps) => {
   return (
-    <CategoryItemLayout to={`/store?category=${text}`}>
+    <CategoryItemLayout to={`/store?category=${categoryMap[text]}`}>
       <CategoryItemImg src={image} />
       <CategoryItemText>{text}</CategoryItemText>
     </CategoryItemLayout>
@@ -16,3 +17,17 @@ const CategoryItem = ({ category: { image, text } }: CategoryItemProps) => {
 };
 
 export default CategoryItem;
+
+const categoryMap: { [key: string]: "TOP6" | "ALL" | category } = {
+  베스트: "TOP6",
+  전체: "ALL",
+  식품: "FOOD",
+  주방: "KITCHEN",
+  욕실: "BATH",
+  생활: "LIFE",
+  취미: "HOBBY",
+  선물: "GIFT",
+  여성용품: "WOMAN",
+  반려동물: "PET",
+  문구: "STATIONERY",
+};
