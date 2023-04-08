29f434b4be75a42f38a964c8b941e89415d3b32f
김상두 <puki4416@gmail.com>
2023-04-08 11:33:32 +0900

    fix : 일부 레이아웃이 안맞는 문제 수정

--- src/components/Store/ProductDetail/Detail/Recommend/Recommend.styles.ts
+++ src/components/Store/ProductDetail/Detail/Recommend/Recommend.styles.ts
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const RecommendLayout = styled.div`
   width: 100%;
   padding: 100px 0;
-  margin-bottom: 300px;
+  margin-bottom: 150px;
 `;
 
 export const RecommendTitle = styled.div`
-  max-width: 1200px;
+  max-width: 1320px;
   margin: 0 auto 40px auto;
   font-size: 24px;
   font-weight: 700;
@@ -17,7 +17,7 @@ export const RecommendTitle = styled.div`
 export const RecommendList = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 10px;
-  max-width: 1200px;
+  gap: 20px;
+  max-width: 1320px;
   margin: auto;
 `;
