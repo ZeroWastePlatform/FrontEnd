ca8a20d195081330abc7c7f8b8bbd422945e98f2
김상두 <puki4416@gmail.com>
2023-04-23 17:48:43 +0900

    feat: 개발환경과 빌드환경 vite 추가

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-styled-components",
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+    envCompatible({ prefix: "" }),
+  ],
+});
